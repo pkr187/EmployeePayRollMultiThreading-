fc7f77c304853af86fbe1649635b71da354894ba
pkr187 <pankajroman187@gmail.com>
2022-11-17 23:19:57 +0530

    [pankaj] Add:MultiThread

---
+++ Multithread/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Multithread
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenEmployeeAddedtotest()
+        {
+            List<EmployeeDetails> empdetails = new List<EmployeeDetails>();
+            {
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "pankaj", "7898323", "Manager"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "Aman", "7898323", "Hr"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "Shivam", "7898323", "Technical"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "Shubham", "7898323", "Admin"));
+                empdetails.Add(new EmployeeDetails(employeeID: 1, employeeName: "Rahul", "7898323", "Fresher"));
+
+                Employeepeyrolloperation employeepayroll = new Employeepeyrolloperation();
+                employeepayroll.AddEmployeeToPayroll(empdetails);
+
+            }
+        }
+    }
+}
